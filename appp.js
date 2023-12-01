5bfaa315ca7a25b896e63072f9f5d6e5ce8f72f7
muhammadusman907 <muhammadusman90712@gmail.com>
2023-12-01 20:40:31 +0500

    calculator

--- appp.js
+++ appp.js
@@ -1,55 +1,62 @@
-var screen = document.querySelectorAll("#screen")[0];
-console.log(screen)
+// var screen = document.querySelectorAll("#screen")[0];
+// console.log(screen)
 
-var operator;
-var num1 = "0";
-var num2 = "0";
+// var operator;
+// var num1 = "0";
+// var num2 = "0";
 
-function num(numParameter) {
-    if (operator) {
-        //work for num2
-        num2 = num2 + numParameter;
-        screen.value = Number(num2)
-    }
-    else {
-        //work for num1
-        num1 = num1 + numParameter;
-        screen.value = Number(num1);
-    }
+// function num(numParameter) {
+//     if (operator) {
+//         //work for num2
+//         num2 = num2 + numParameter;
+//         screen.value = Number(num2)
+//     }
+//     else {
+//         //work for num1
+//         num1 = num1 + numParameter;
+//         screen.value = Number(num1);
+//     }
+// }
+
+// function setOperator(operatorParameter) {
+//     console.log(operatorParameter)
+//     operator = operatorParameter;
+
+//     screen.value = 0;
+// }
+
+// function calculate() {
+//     if (operator === "+") { 
+//     screen.value = Number(num1) + Number(num2);
+//     }
+//     else if(operator === "-"){
+//         screen.value = Number(num1) - Number(num2);
+//     }
+//     else if(operator === "x"){
+//         screen.value = Number(num1) * Number(num2);
+//     }
+//     else if(operator === "/"){
+//         screen.value = Number(num1) / Number(num2);
+//     }
+// }
+
+
+// function clearAll(){
+//     operator = undefined;
+//     num1 = "0";
+//     num2 = "0";
+//     screen.value = "0";
+// }
+
+
+// Calculator Code End Here 
+
+var input = document.getElementById("input")
+function num(number){
+   input.value += number;
+} 
+
+function setOperator(opperator) {
+    input.value += opperator ; 
+    console.log(input.value[input.value - 2])
 }
-
-function setOperator(operatorParameter) {
-    console.log(operatorParameter)
-    operator = operatorParameter;
-
-    screen.value = 0;
-}
-
-function calculate() {
-    if (operator === "+") { 
-    screen.value = Number(num1) + Number(num2);
-    }
-    else if(operator === "-"){
-        screen.value = Number(num1) - Number(num2);
-    }
-    else if(operator === "x"){
-        screen.value = Number(num1) * Number(num2);
-    }
-    else if(operator === "/"){
-        screen.value = Number(num1) / Number(num2);
-    }
-}
-
-
-function clearAll(){
-    operator = undefined;
-    num1 = "0";
-    num2 = "0";
-    screen.value = "0";
-}
-
-
-// Calculator Code End Here
-
-
-
